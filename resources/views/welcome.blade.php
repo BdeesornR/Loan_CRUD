479f73323a4dea2771ec121a1a3e7a8cff4cf670
Bdeesorn <bdeesorn@peerpower.co.th>
2021-10-20 14:58:11 +0700

    3rd commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,11 @@
 @extends('layout.layout')
 
+<!-- Testing -->
+
 @section('content')
 
     <div class="content">
-        <h1>Loans Application</h1>   
+        <h1>Loans Application</h1>
     </div>
 
 @endsection
